c73f435de7e5b92f66cdd3bb05dabcc72fef8abc
PengRong <1557862201@qq.com>
2019-05-31 15:18:51 +0800

    Restful CRUD JPA  新增logback日志组件，数据源配置，MGB mapper 单表映射自动生成

--- restfulcrudjpa/src/main/java/org/vincent/springbootstudy/RestfulcrudjpsApplication.java
+++ restfulcrudjpa/src/main/java/org/vincent/springbootstudy/RestfulcrudjpsApplication.java
@@ -1,21 +1,38 @@
 package org.vincent.springbootstudy;
 
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.vincent.springbootstudy.dao.impl.TUserMapper;
+import org.vincent.springbootstudy.dao.model.TUser;
+import org.vincent.springbootstudy.dao.model.TUserExample;
+
+import java.util.List;
 
 @SpringBootApplication
 //@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 @RestController
+@MapperScan("org.vincent.springbootstudy.dao.impl")
 public class RestfulcrudjpsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RestfulcrudjpsApplication.class, args);
-	}
-	@RequestMapping(path = "/")
-	public String index(){
-		return "hello Restful.";
-	}
+    @Autowired
+    private TUserMapper tUserMapper;
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestfulcrudjpsApplication.class, args);
+    }
+
+    @RequestMapping(path = "/")
+    public String index() {
+        /** 访问数据库 */
+        TUserExample example = new TUserExample();
+        example.createCriteria().andIdEqualTo(1);
+        List<TUser> tUsers = tUserMapper.selectByExample(example);
+        System.out.println(tUsers);
+        return "hello Restful.";
+    }
 
 }
