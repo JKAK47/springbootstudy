3f93b07b29fe91a52a5803f469f3180e0d371b87
PengRong <1557862201@qq.com>
2019-05-31 14:40:24 +0800

    Restful CRUD JPA  新增logback日志组件，数据源配置，MGB mapper 单表映射自动生成

--- restfulcrudjpa/src/main/java/org/vincent/springbootstudy/RestfulcrudjpsApplication.java
+++ restfulcrudjpa/src/main/java/org/vincent/springbootstudy/RestfulcrudjpsApplication.java
@@ -2,12 +2,20 @@ package org.vincent.springbootstudy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+//@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
+@RestController
 public class RestfulcrudjpsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestfulcrudjpsApplication.class, args);
+	}
+	@RequestMapping(path = "/")
+	public String index(){
+		return "hello Restful.";
 	}
 
 }
