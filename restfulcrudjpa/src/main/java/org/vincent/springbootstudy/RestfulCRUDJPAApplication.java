f204526c86167d7b2dbbf15cd4ccab39778f31d4
PR <1557862201@qq.com>
2019-06-06 00:42:37 +0800

    springboot 父pom 新增 子pom

--- restfulcrudjpa/src/main/java/org/vincent/springbootstudy/RestfulCRUDJPAApplication.java
+++ restfulcrudjpa/src/main/java/org/vincent/springbootstudy/RestfulCRUDJPAApplication.java
@@ -1,5 +1,6 @@
 package org.vincent.springbootstudy;
 
+import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
@@ -16,13 +17,16 @@ import java.util.List;
 //@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 @RestController
 @MapperScan("org.vincent.springbootstudy.dao.impl")
+@Slf4j
 public class RestfulCRUDJPAApplication {
 
     @Autowired
     private TUserMapper tUserMapper;
 
     public static void main(String[] args) {
+        log.info("begin >>> start SpringBoot.");
         SpringApplication.run(RestfulCRUDJPAApplication.class, args);
+        log.debug("sucessfully  >>> start SpringBoot");
     }
 
     @RequestMapping(path = "/")
